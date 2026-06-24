d07d031725c9e49909aad07d7f479478b4219a4d
Lautaro Valentino Guzzio <lauguzzio@gmail.com>
2026-06-24 02:44:14 -0300

    switch hardcoded response to db request

--- backend/controllers/api.controller.js
+++ backend/controllers/api.controller.js
@@ -1,13 +1,10 @@
-exports.getProductosActivos = (req, res) => {
+const { sequelize, Producto, Venta, DetalleVenta } = require('../models/index');
+
+exports.getProductosActivos = async (req, res) => {
     try {
-        return res.status(200).json({
-            status: "success",
-            data: { id: 1, nombre: "sas", categoria: "producto", precio: 999, imagen: "foo.jpg", activo: true }
-        });
-    } catch (error) {
-        return res.status(500).json({
-            status: "error",
-            mensaje: "Error al obtener los productos de la tienda"
-        });
+        const productos = await Producto.findAll({ where: { activo: true } });
+        return res.status(200).json({ status: "success", data: productos });
+    } catch (e) {
+        return res.status(500).json({ status: "error", mensaje: e.message });
     }
 };
